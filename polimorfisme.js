758d7aeeae1deaf62da220df971a8bd2396a72e5
Gizza F Nadifa <geeznotgizz@gmail.com>
2024-10-08 09:34:26 +0700

    Update polimorfisme.js

--- polimorfisme.js
+++ polimorfisme.js
@@ -18,11 +18,18 @@ class Design extends Orang {
     }
 }
 
+class DataAnalyst extends Orang {
+    tampilkanPeran() {
+        console.log("Peran: Data Analyst ")
+    }
+}
 // Menggunakan polimorfisme
 const orang1 = new Orang();
 const mahasiswa1 = new Mahasiswa();
 const design1 = new Design();
+const dataanalyst1 = new DataAnalyst();
 
 orang1.tampilkanPeran();    // Output: Peran: Orang Kaya
 mahasiswa1.tampilkanPeran(); // Output: Peran: Mahasiswa
 design1.tampilkanPeran();     // Output: Peran: Graphic Designer
+dataanalyst1.tampilkanPeran(); //Output: Peran: Data Analyst
